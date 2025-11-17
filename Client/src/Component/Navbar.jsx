a924bf3775ba6f12b2b94247aa14d256a282c89e
adityasinghchouhan1 <asadityasingh2572000@gmail.com>
2025-11-17 11:12:26 +0530

    create Therapy

--- Client/src/Component/Navbar.jsx
+++ Client/src/Component/Navbar.jsx
@@ -16,18 +16,18 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="fixed top-0 left-0 w-full z-20 flex justify-between items-center pr-2 py-6 bg-black/55 text-white">
+      <nav className="fixed top-0 left-0 w-full z-20 flex justify-between items-center pr-2 py-3 bg-white text-[#0b223d">
         {/* Logo */}
         <div className="bg-gradient-to-r rounded-sm from-green-200 flex justify-center items-center">
           <img
             src={logo}
             className="w-16 bg-center bg-no-repeat object-fit-cover bg-cover"
           />
-          <h1 className="text-black font-Yellowtail text-3xl">Aayurvaan</h1>
+          <h1 className="text-[#0b223d] font-Yellowtail text-3xl">Aayurvaan</h1>
         </div>
 
         {/* DESKTOP MENU */}
-        <ul className="hidden md:flex gap-12 font-bold text-white">
+        <ul className="hidden md:flex gap-12 font-bold text-[#0b223d]">
           {[
             { name: 'Home', path: '' },
             { name: 'About', path: 'About' },
@@ -64,7 +64,7 @@ const Navbar = () => {
 
         {/* MOBILE SLIDE MENU */}
         <div
-          className={`md:hidden fixed z-[9999] top-0 left-0 text-white h-full bg-black/25 transition-all duration-300 overflow-hidden font-serif border-r-4 border-green-500 ${
+          className={`md:hidden fixed z-[9999] top-0 left-0 text-[#0b223d] h-full bg-black/25 transition-all duration-300 overflow-hidden font-serif border-r-4 border-green-500 ${
             isOpen ? 'w-56 backdrop-blur-sm' : 'w-0'
           }`}
         >
@@ -82,7 +82,7 @@ const Navbar = () => {
                 onClick={CloseButton}
                 className="
                   relative w-full py-2 
-                  transition-all duration-200 text-white text-center
+                  transition-all duration-200 text-[#0b223d] text-center
                   hover:bg-black/40
                   after:content-[''] after:absolute after:left-1/2 after:-bottom-0
                   after:h-[2px] after:w-0 after:bg-green-500 
