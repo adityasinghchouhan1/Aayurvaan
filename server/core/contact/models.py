2e250eea46056ba57200a1ca7adfa669e07b7459
adityasinghchouhan1 <asadityasingh2572000@gmail.com>
2026-01-24 11:44:14 +0530

    create models of Contact form in contact

--- server/core/contact/models.py
+++ server/core/contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+    contact=models.CharField(max_length=15)
+    message=models.TextField(auto_now_add=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
