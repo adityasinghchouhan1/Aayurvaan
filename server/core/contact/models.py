45428a68c7b5c1b625b981343c6aa9d581763bc1
adityasinghchouhan1 <asadityasingh2572000@gmail.com>
2026-01-24 11:45:21 +0530

    edit

--- server/core/contact/models.py
+++ server/core/contact/models.py
@@ -5,7 +5,7 @@ class Contact(models.Model):
     name=models.CharField(max_length=100)
     email=models.EmailField()
     contact=models.CharField(max_length=15)
-    message=models.TextField(auto_now_add=True)
+    message=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
