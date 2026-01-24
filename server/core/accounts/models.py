b8888d13eeb4b7e94d5ec607f7e5a6095df893dc
adityasinghchouhan1 <asadityasingh2572000@gmail.com>
2026-01-24 20:31:20 +0530

    Update user email in accounts model for testing purposes

--- server/core/accounts/models.py
+++ server/core/accounts/models.py
@@ -1,3 +1,5 @@
-from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+user = User.objects.get(username='Ayuvan')   # apna username daalo
+user.email = 'admin@gmail.com'
+user.save()
