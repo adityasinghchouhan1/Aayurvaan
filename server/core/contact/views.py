32041f137d04eebc0ffab80a25bbacdc5ccdf5ba
adityasinghchouhan1 <asadityasingh2572000@gmail.com>
2026-01-24 16:37:48 +0530

    Implement ContactCreateAPIView for handling contact form submissions

--- server/core/contact/views.py
+++ server/core/contact/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from .serializers import ContactSerializer
 # Create your views here.
+
+class ContactCreateAPIView(APIView):
+  permission_classes = [AllowAny]
+
+  def post(self, request):
+    serializers = ContactSerializer(data=request.data)
+    if serializers.is_valid():
+      serializers.save()
+      return Response({"message":"Contact Saved Succesfully "})
+    return Response(serializers.errors, status=400)
