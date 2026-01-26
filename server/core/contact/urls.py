0958a688bc1676e13bd9cca7c69c5cee5ea927b2
adityasinghchouhan1 <asadityasingh2572000@gmail.com>
2026-01-26 21:29:36 +0530

    Add URL patterns for ContactListAPIView and ContactDetailAPIView

--- server/core/contact/urls.py
+++ server/core/contact/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ContactCreateAPIView
+from .views import ContactCreateAPIView , ContactDetailAPIView, ContactListAPIView
 
 urlpatterns = [
     path('contact/',ContactCreateAPIView.as_view()),
+    path('contacts/', ContactListAPIView.as_view()),
+path('contacts/<int:pk>/', ContactDetailAPIView.as_view()),
+
 ]
