e8b8f987329663c80f670998e6508798224f4ff4
adityasinghchouhan1 <asadityasingh2572000@gmail.com>
2026-01-28 17:36:18 +0530

    slider models

--- server/core/slider/models.py
+++ server/core/slider/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Slider(models.Model):
+   title=models.CharField(max_length=200)
+   image=models.ImageField(upload_to='sliders/')
+   description=models.TextField(blank=True)
+   is_active=models.BooleanField(default=True)
+   created_at=models.DateTimeField(auto_now_add=True)
+
+   def __str__(self):
+      return self.title
