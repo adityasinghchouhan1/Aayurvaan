712f8d0b0731f74e8dcd1a150656daf37d96b340
adityasinghchouhan1 <asadityasingh2572000@gmail.com>
2026-02-02 16:58:13 +0530

    Models of therapy

--- server/core/therapy/models.py
+++ server/core/therapy/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class therapy(models.Model):
+  Title=models.CharField(max_length=200)
+  file=models.ImageField(upload_to='therapy/')
+  description=models.TextField(blank=True)
+  is_active=models.BooleanField(default=True)
+  created_at=models.DateTimeField(auto_now_add=True)
+
+
+  def __str__(self):
+    return self.Title
