1abf6b75521f8f0da966ebc849a69fb523332242
adityasinghchouhan1 <asadityasingh2572000@gmail.com>
2026-02-12 21:33:12 +0530

    imp

--- server/core/order/views.py
+++ server/core/order/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.db import transaction
 
+from .models import Order,OrderItem
+from product.models import Product
 # Create your views here.
+
+class CreateOrderAPIView(APIView):
+  permission_classes=[IsAuthenticated]
+
+  def post(self,request):
+    
