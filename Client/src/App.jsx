804554bea93abe214f7be623277e50bc5d637bd2
adityasinghchouhan1 <asadityasingh2572000@gmail.com>
2026-02-23 21:15:57 +0530

    feat: implement user authentication flow on app initialization

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 import Navbar from '../src/Component/Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from './Component/Footer/Footer'
 import ScrollToTop from './Component/ScrollToTop' // 👈 import it
+import axiosInstance from './common/axiosInstance'
+import { loginSuccess, logout } from './redux/authSlice'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const token = localStorage.getItem('access')
+
+    if (token) {
+      axiosInstance
+        .get('me/')
+        .then((res) => {
+          dispatch(
+            loginSuccess({
+              user: res.data,
+              token: token,
+            }),
+          )
+        })
+        .catch(() => {
+          dispatch(logout())
+        })
+    }
+  })
   return (
     <div className="relative min-h-screen">
       <ScrollToTop /> {/* 👈 add this before rendering routes */}
