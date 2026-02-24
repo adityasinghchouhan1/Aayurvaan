cd5a227862cd1ef4dad10ffd7d0b19c081e1cd39
adityasinghchouhan1 <asadityasingh2572000@gmail.com>
2026-02-24 22:29:29 +0530

    urls

--- server/core/clientuser/urls.py
+++ server/core/clientuser/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MeAPIView, RegisterAPIView, LoginAPIView
+from .views import AdminUserListAPIView, MeAPIView, RegisterAPIView, LoginAPIView
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view()),
     path('login/', LoginAPIView.as_view()),
     path('me/', MeAPIView.as_view(), name='me'),
+    path('admin/users/', AdminUserListAPIView.as_view(), name='admin-users'),
 
 ]
