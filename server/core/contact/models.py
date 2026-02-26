cae79e2e47c1f94683cdfd08e127bbf364d436a7
adityasinghchouhan1 <asadityasingh2572000@gmail.com>
2026-02-26 20:07:23 +0530

    add status field in contect models

--- server/core/contact/models.py
+++ server/core/contact/models.py
@@ -6,7 +6,16 @@ class Contact(models.Model):
     email=models.EmailField()
     contact=models.CharField(max_length=15)
     message=models.TextField()
+    status = models.CharField(
+    max_length=20,
+    choices=[
+        ("pending", "Pending"),
+        ("resolved", "Resolved"),
+    ],
+    default="pending"
+)
     created_at=models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.name
